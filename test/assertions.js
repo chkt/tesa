4288fe7f43c8e72d93fdc9231552ec31097a0df2
chkt <ck@christoph-kettelhoit.de>
2016-08-06 00:11:27 +0200

    Added tests for assertions

---
+++ test/assertions.js
@@ -0,0 +1,73 @@
+import assert from 'assert';
+import { describe, it } from 'mocha';
+
+import * as assertions from '../source/assertions';
+
+
+
+describe("assertions", () => {
+	describe("get", () => {
+		it("should return an object containing a throw and a return property", () => {
+			const ret = assertions.get();
+
+			assert.strictEqual(typeof ret.return, 'function');
+			assert.strictEqual(typeof ret.throw, 'function');
+		});
+
+		it("should contain the default assertion functions", () => {
+			const ret = assertions.get();
+
+			ret.return(() => true, []);
+
+			ret.throw(() => {
+				throw new Error();
+			}, []);
+		});
+	});
+
+	describe("set", () => {
+		it("should require two functions as arguments", () => {
+			const set = assertions.set;
+			const fn = () => 1;
+
+			assert.throws(() => set());
+			assert.throws(() => set(null));
+			assert.throws(() => set(true));
+			assert.throws(() => set(1));
+			assert.throws(() => set("1"));
+			assert.throws(() => set(Symbol(1)));
+			assert.throws(() => set(/^1$/));
+			assert.throws(() => set({ "1" : 1 }));
+			assert.throws(() => set([1]));
+			assert.throws(() => set(fn));
+			assert.throws(() => set(fn, null));
+			assert.throws(() => set(fn, true));
+			assert.throws(() => set(fn, 1));
+			assert.throws(() => set(fn, "1"));
+			assert.throws(() => set(fn, Symbol(1)));
+			assert.throws(() => set(fn, /^1$/));
+			assert.throws(() => set(fn, { "1" : 1 }));
+			assert.throws(() => set(fn, [1]));
+			assert.doesNotThrow(() => set(fn, fn));
+		});
+
+		it("should set the return and the throw assertion functions", () => {
+			const fnA = (fn, args) => 1;
+			const fnB = (fn, args) => 1;
+
+			assertions.set(fnA, fnB);
+
+			let ret = assertions.get();
+
+			assert.strictEqual(ret.return, fnA);
+			assert.strictEqual(ret.throw, fnB);
+
+			assertions.set(fnB, fnA);
+
+			ret = assertions.get();
+
+			assert.strictEqual(ret.return, fnB);
+			assert.strictEqual(ret.throw, fnA);
+		});
+	});
+});
