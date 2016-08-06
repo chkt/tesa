a0351ff00b2a9f90a3284ea6c8f8808044109b5b
chkt <ck@christoph-kettelhoit.de>
2016-08-06 11:12:53 +0200

    Added argument tests for use()

--- test/argumentGenerator.js
+++ test/argumentGenerator.js
@@ -1,7 +1,9 @@
 import assert from 'assert';
 
 import { describe, it } from 'mocha';
+
 import use, * as u from '../source/argumentGenerator';
+import * as assertions from '../source/assertions';
 
 
 
@@ -42,5 +44,65 @@ describe("u", () => {
 
 		assert.strictEqual(typeof u.TYPE_FUNCTION, 'symbol');
 		assert.strictEqual(typeof u.TYPE_FUNCTION_GENERATOR, 'symbol');
+	});
+});
+
+
+describe("use", () => {
+	it("should require its last argument to be a function", () => {
+		const arg = [ null ];
+		const fn = (fn, args) => null;
+
+		assertions.set(fn, fn);
+
+		assert.throws(() => use(), TypeError);
+		assert.throws(() => use(null), TypeError);
+		assert.throws(() => use(true), TypeError);
+		assert.throws(() => use(1), TypeError);
+		assert.throws(() => use("1"), TypeError);
+		assert.throws(() => use(Symbol("1")), TypeError);
+		assert.throws(() => use(/^1$/), TypeError);
+		assert.throws(() => use({ "1" : 1 }), TypeError);
+		assert.throws(() => use([ 1 ]), TypeError);
+		assert.doesNotThrow(() => use(() => 1));
+		assert.throws(() => use(arg, null), TypeError);
+		assert.throws(() => use(arg, true), TypeError);
+		assert.throws(() => use(arg, 1), TypeError);
+		assert.throws(() => use(arg, "1"), TypeError);
+		assert.throws(() => use(arg, Symbol("1")), TypeError);
+		assert.throws(() => use(arg, /^1$/), TypeError);
+		assert.throws(() => use(arg, { "1" : 1 }), TypeError);
+		assert.throws(() => use(arg, [ 1 ]), TypeError);
+		assert.doesNotThrow(() => use(arg, () => 1));
+	});
+
+	it("should require all additional arguments to be nonempty arrays", () => {
+		const assertfn = (fn, args) => null;
+		const testfn = () => 1;
+
+		assertions.set(assertfn, assertfn);
+
+		assert.throws(() => use(undefined, testfn), TypeError);
+		assert.throws(() => use(null, testfn), TypeError);
+		assert.throws(() => use(true, testfn), TypeError);
+		assert.throws(() => use(1, testfn), TypeError);
+		assert.throws(() => use("1", testfn), TypeError);
+		assert.throws(() => use(/^1$/, testfn), TypeError);
+		assert.throws(() => use(Symbol("1"), testfn), TypeError);
+		assert.throws(() => use(() => 1, testfn), TypeError);
+		assert.throws(() => use({ "1" : 1 }, testfn), TypeError);
+		assert.throws(() => use([], testfn), TypeError);
+		assert.doesNotThrow(() => use([ null], testfn));
+		assert.throws(() => use([ null ], undefined, testfn), TypeError);
+		assert.throws(() => use([ null ], null, testfn), TypeError);
+		assert.throws(() => use([ null ], true, testfn), TypeError);
+		assert.throws(() => use([ null ], 1, testfn), TypeError);
+		assert.throws(() => use([ null ], "1", testfn), TypeError);
+		assert.throws(() => use([ null ], /^1$/, testfn), TypeError);
+		assert.throws(() => use([ null ], Symbol("1"), testfn), TypeError);
+		assert.throws(() => use([ null ], () => 1, testfn), TypeError);
+		assert.throws(() => use([ null ], { "1" : 1 }, testfn), TypeError);
+		assert.throws(() => use([ null ], [], testfn), TypeError);
+		assert.doesNotThrow(() => use([ null ], [ null], testfn));
 	});
 });
